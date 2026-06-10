eef7f2504329a2e218d2a94c5baf7ba38ea81fac
Joshua Zillwood <joshua.zillwood@gmail.com>
2026-06-09 21:15:09 -0500

    feat: add XenoCantoError and npm package README (#12)
    * feat(error): add XenoCantoError with status code
    
    * feat(error): export XenoCantoError from package
    
    * docs: add npm package README

---
+++ lib/README.md
@@ -0,0 +1,15 @@
+# Xeno-Canto-Api
+
+[![NPM Version](https://img.shields.io/npm/v/xeno-canto-api)](https://www.npmjs.com/package/xeno-canto-api) [![NPM Downloads](https://img.shields.io/npm/d18m/xeno-canto-api)](https://www.npmjs.com/package/xeno-canto-api)
+
+## Summary
+
+A TypeScript client library for the [Xeno Canto API v3](https://xeno-canto.org/explore/api).
+
+## Installation
+
+    npm install xeno-canto-api
+
+## Usage
+
+Documentation can be found [here](https://github.com/jzills/Xeno-Canto-Api/blob/main/README.md).
--- lib/src/xeno-canto.ts
+++ lib/src/xeno-canto.ts
@@ -1,6 +1,13 @@
 import QueryBuilder from "./builders/query-builder";
 import { RecordingsResponse } from "./types/recordings-response";
 
+export class XenoCantoError extends Error {
+    constructor(readonly status: number, message: string) {
+        super(message);
+        this.name = "XenoCantoError";
+    }
+}
+
 export default class XenoCanto {
     readonly URL: string = "https://xeno-canto.org/api/3";
     readonly APIKey: string;
@@ -14,7 +21,7 @@ export default class XenoCanto {
         if (response.ok) {
             return response.json();
         } else {
-            throw new Error(await response.text());
+            throw new XenoCantoError(response.status, await response.text());
         }
     };
 }
